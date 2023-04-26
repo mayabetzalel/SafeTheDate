c49688ffb2d8df34d2af3ef8b6f9c73de9edf10b
EdenItz <edenitz34@gmail.com>
2023-04-26 18:50:39 +0300

    upload qr

--- client/src/components/ImportTicket.tsx
+++ client/src/components/ImportTicket.tsx
@@ -1,9 +1,67 @@
-import { Paper, Typography } from "@mui/material";
+import { Button, Grid, Paper } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Delete';
+import UploadIcon from '@mui/icons-material/Upload';
+import QrReader from 'react-qr-scanner'
+import { Box } from '@mui/system';
+import { useState } from 'react';
+
+interface scanData {
+  text: string
+  canvas: object
+  format: number
+  numBits: number
+  rawBytes: any
+  resultMetadata: any
+  timestamp: any
+  resultPoints: any
+}
 
 export const ImportTicket = () => {
+  const [delay, setDelay] = useState(100)
+  const [result, setResult] = useState<scanData | null>(null)
+
+  const handleScan = (data: scanData) => {
+    console.log(data)
+    setResult(data)
+  }
+  const handleError = (err: object) => {
+    console.error(err)
+  }
+
   return (
-    <Paper>
-      <Typography variant={"h4"}>Import Ticket</Typography>
-    </Paper>
+    <Grid container rowSpacing={2} direction='column' sx={{ placeItems: 'center' }}>
+
+      <Grid item>
+        <QrReader
+          style={{ width: '20em', height: '20em', background: 'pink' }}
+          delay={delay}
+          onError={handleError}
+          onScan={handleScan}
+        />
+        <p>{result?.text}</p>
+      </Grid>
+
+      <Grid container item spacing={2} direction='row' sx={{ placeItems: 'center' }}>
+        <Grid item sx={{ placeItems: 'center' }}>
+          <Button variant="outlined" color="secondary" startIcon={<ClearIcon />}>
+            Clear Code
+          </Button>
+        </Grid>
+
+        <Grid item sx={{ placeItems: 'center' }}>
+          <Button variant="contained" color="secondary" startIcon={<UploadIcon />}>
+            Upload Code
+          </Button>
+        </Grid>
+      </Grid>
+
+      <Grid item justifyContent='center' alignItems='center'>
+        <Button variant="contained" color="secondary">
+          Validate
+        </Button>
+      </Grid>
+    </Grid>
   );
-};
+}
+
+export default ImportTicket;
