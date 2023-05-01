5fa534a0f4cda6d96f1a5337df20c75db12be0e3
EdenItz <edenitz34@gmail.com>
2023-05-01 20:49:34 +0300

    only event scan

--- client/src/components/ImportTicket.tsx
+++ client/src/components/ImportTicket.tsx
@@ -17,50 +17,9 @@ interface scanData {
 }
 
 export const ImportTicket = () => {
-  const [delay, setDelay] = useState(100)
-  const [result, setResult] = useState<scanData | null>(null)
-
-  const handleScan = (data: scanData) => {
-    console.log(data)
-    setResult(data)
-  }
-  const handleError = (err: object) => {
-    console.error(err)
-  }
 
   return (
-    <Grid container rowSpacing={2} direction='column' sx={{ placeItems: 'center' }}>
-
-      <Grid item>
-        <QrReader
-          style={{ width: '20em', height: '20em', background: 'pink' }}
-          delay={delay}
-          onError={handleError}
-          onScan={handleScan}
-        />
-        <p>{result?.text}</p>
-      </Grid>
-
-      <Grid container item spacing={2} direction='row' sx={{ placeItems: 'center' }}>
-        <Grid item sx={{ placeItems: 'center' }}>
-          <Button variant="outlined" color="secondary" startIcon={<ClearIcon />}>
-            Clear Code
-          </Button>
-        </Grid>
-
-        <Grid item sx={{ placeItems: 'center' }}>
-          <Button variant="contained" color="secondary" startIcon={<UploadIcon />}>
-            Upload Code
-          </Button>
-        </Grid>
-      </Grid>
-
-      <Grid item justifyContent='center' alignItems='center'>
-        <Button variant="contained" color="secondary">
-          Validate
-        </Button>
-      </Grid>
-    </Grid>
+    <div>import ticket</div>
   );
 }
 
