7ba0094cd2d0c34858a43485505005a72db432a8
avivtsadik <48044366+avivtsadik@users.noreply.github.com>
2023-05-15 14:57:02 +0300

    fix

--- client/src/hooks/GraphqlClientProvider/GraphqlClientProvider.tsx
+++ client/src/hooks/GraphqlClientProvider/GraphqlClientProvider.tsx
@@ -1,15 +1,13 @@
-import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
-import {PropsWithChildren} from "react";
+import { Client, Provider, cacheExchange, fetchExchange } from "urql";
+import { PropsWithChildren } from "react";
 
 const client = new Client({
-    url: 'http://localhost:4000/graphql',
-    exchanges: [cacheExchange, fetchExchange],
+  url: process.env.REACT_APP_BACKEND_URL + "/graphql",
+  exchanges: [cacheExchange, fetchExchange],
 });
 
-const GraphqlClientProvider = ({children}: PropsWithChildren) => (
-    <Provider value={client}>
-        {children}
-    </Provider>
+const GraphqlClientProvider = ({ children }: PropsWithChildren) => (
+  <Provider value={client}>{children}</Provider>
 );
 
-export default GraphqlClientProvider
+export default GraphqlClientProvider;
