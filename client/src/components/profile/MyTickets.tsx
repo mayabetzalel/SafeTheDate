65f1164a9f79274f4cda2f0702e949333ee555b2
thcr <talmachlev3399@gmail.com>
2023-05-25 23:03:33 +0300

    extra changes

--- client/src/components/profile/MyTickets.tsx
+++ client/src/components/profile/MyTickets.tsx
@@ -2,7 +2,7 @@ import { gql, useQuery } from "urql";
 import FetchingState from "../../utils/fetchingState";
 import EventCard from "../EventCard/EventCard";
 import { graphql } from "../../graphql";
-import { Event, Exact, FilterEventParams, TicketResponse } from "../../graphql/graphql";
+import { Event, Exact, FilterEventParams, InputTicket, TicketResponse } from "../../graphql/graphql";
 import { Grid, Pagination, PaginationItem } from "@mui/material";
 import { MutableRefObject, useEffect, useRef, useState } from "react";
 import styled from "@emotion/styled";
@@ -11,6 +11,7 @@ import { RoutePaths } from "../../App";
 import { floor } from "lodash";
 import { MyTicket } from "./MyTicket";
 import { useAuth } from "../../hooks/authController/AuthContext";
+import CreateTicket from "../DisplayTicketUsingEvent";
 
 const GridHiddenScroll = styled(Grid)({
   "::-webkit-scrollbar": {
@@ -33,6 +34,7 @@ const ticketQuery = graphql(`
       type
       image
       onMarketTime
+      barcode
     }
   }
 `);
@@ -52,9 +54,10 @@ interface TicketsProps {
 
 const MyTickets = ({ filterParams, customerId }: TicketsProps) => {
   const { currentUser } = useAuth();
+  const [tickets, setTickets] = useState<TicketResponse[]>([]);
   const [page, setPage] = useState(0);
   const navigate = useNavigate();
-  const [{ data = { ticket: [] }, fetching, error }, reexecuteQuery] = useQuery<
+  const [{ data = { ticket: [] }, fetching, error }] = useQuery<
     { ticket: Exact<TicketResponse>[] },
     { filterParams: FilterEventParams; skip: number; limit: number; customerId: string; }
   >({
@@ -79,31 +82,40 @@ const MyTickets = ({ filterParams, customerId }: TicketsProps) => {
   });
 
   function localUpdateMarketTime(ticketId: string) {
-    // const ticketIndex = data.ticket.findIndex(ticket => ticket.ticketId === ticketId);
-    // data.ticket[ticketIndex].onMarketTime = data.ticket[ticketIndex].onMarketTime ? null : new Date().getTime(); 
-    reexecuteQuery();
+    setTickets(prev => {
+      const ticketIndex = prev.findIndex(ticket => ticket.ticketId === ticketId);
+
+      let updatedTickets = [...prev];
+
+      updatedTickets[ticketIndex].onMarketTime = prev[ticketIndex].onMarketTime ? 0 : new Date().getTime(); 
+      return updatedTickets;
+    })
   }
 
   useEffect(() => {
     setPage(0);
   }, [filterParams]);
 
+  useEffect(() => {
+    setTickets(data.ticket)
+  }, [data.ticket])
+
   return (
     <FetchingState isFetching={fetching}>
+
       <GridHiddenScroll container sx={{ height: "inherit", overflowY: "auto" }}>
-        {data.ticket.map(({ ticketId, name, type, location, timeAndDate, image, onMarketTime }) => (
-          <Grid key={ticketId!} item sm={4} md={3}>
+        {tickets.map((ticket) => {
+          const { ticketId, onMarketTime } = ticket;
+          return <Grid key={ticketId!} item sm={4} md={3}>
             <MyTicket
-              title={name!}
-              header={type!}
-              subheader={location!}
-              image={image || undefined}
-              id={ticketId?.toString() || ""}
-              localUpdateMarketTime = {localUpdateMarketTime}
+              ticket={ticket}
+              localUpdateMarketTime={localUpdateMarketTime}
               isOnMarket={!!onMarketTime}
             />
           </Grid>
-        ))}
+
+        }
+        )}
       </GridHiddenScroll>
       <Pagination
         count={floor(dataCount?.ticketCount / EVENTS_PER_FETCH) + 1}
