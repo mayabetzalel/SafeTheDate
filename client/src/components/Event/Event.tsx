164c3ab7c6ab163649500ee95bfa613edd1ddf51
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2023-05-29 13:24:42 +0300

    123

--- client/src/components/Event/Event.tsx
+++ client/src/components/Event/Event.tsx
@@ -8,23 +8,24 @@ import {
   Divider,
   Button,
   IconButton,
+  FormControlLabel,
+  Switch,
   Tooltip,
 } from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
+import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner";
 import EventIcon from "@mui/icons-material/Event";
 import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "urql";
 import { graphql } from "../../graphql";
 import FetchingState from "../../utils/fetchingState";
 import { useEffect, useState } from "react";
-import { Exact, Event as EventType } from "../../graphql/graphql";
+import {Exact, Event as EventType, User} from "../../graphql/graphql";
 import PaymentForm from "../checkout/PaymentForm";
 import { useAuth } from "../../hooks/authController/AuthContext";
 import { Login, Logout } from "@mui/icons-material";
 import * as React from "react";
 import { RoutePaths } from "../../App";
-import { Center } from "../../utils/center";
 
 const EVENT_QUERY = graphql(`
   query event($ids: [String]) {
@@ -36,6 +37,7 @@ const EVENT_QUERY = graphql(`
       ticketPrice
       timeAndDate
       description
+      ownerId
       image
     }
   }
@@ -50,17 +52,35 @@ const EVENT_QUERY_IMAGE = graphql(`
   }
 `);
 
+const USER_QUERY = graphql(`
+  query getUserName($userId: String!) {
+    user(userId: $userId) {
+      username
+    }
+  }
+`);
+
 export const Event = () => {
-  const { currentUser } = useAuth();
+  const { currentUser = {} } = useAuth();
   const navigate = useNavigate();
-  const publisherName = "publisher name";
   const [event, setEvent] = useState<Exact<EventType>>();
   const { id = "" } = useParams();
+  const [isInEditMode, setIsInEditMode] = useState(false);
   const [{ data, fetching }] = useQuery<{
     event: Exact<EventType>[];
   }>({
     query: EVENT_QUERY,
     variables: { ids: [id] },
+  });
+
+  const [{ data: userData = { user: {} } }] = useQuery<
+      { user: Pick<User, 'username'> },
+      { userId: string; }
+      >({
+    query: USER_QUERY,
+    variables: {
+      userId: currentUser?.['_id']
+    },
   });
 
   const [dataImage] = useQuery<{
@@ -102,44 +122,70 @@ export const Event = () => {
           xs
         >
           <Stack spacing={2}>
-            <Typography variant={"h3"}>{event?.name}</Typography>
-              <Stack direction={"row"} spacing={2} alignItems={"center"}>
-                <Tooltip title="scan event tickets">
-                  <IconButton onClick={() => navigate(`${RoutePaths.SCAN_EVENT}/${id}`, {})}> <QrCodeScannerIcon fontSize="large" /></IconButton>
-                </Tooltip>
-                <Avatar
-                  sx={(theme) => ({ bgcolor: theme.palette.secondary.main })}
+            <Grid container justifyContent={"space-between"}>
+              <Typography variant={"h3"}>{event?.name}</Typography>
+              {event?.ownerId === currentUser?.["_id"] ? (
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={isInEditMode}
+                      onChange={(e) => setIsInEditMode(e.target.checked)}
+                    />
+                  }
+                  label="Edit Mode"
+                />
+              ) : (
+                <></>
+              )}
+            </Grid>
+            <Stack direction={"row"} spacing={2} alignItems={"center"}>
+              <Avatar
+                sx={(theme) => ({ bgcolor: theme.palette.secondary.main })}
+              >
+                {userData.user.username?.charAt(0)}
+              </Avatar>
+              <Typography variant="h6">{userData.user.username}</Typography>
+              <Tooltip title="scan event tickets">
+                <IconButton
+                    onClick={() => navigate(`${RoutePaths.SCAN_EVENT}/${id}`, {})}
                 >
-                  {publisherName.charAt(0)}
-                </Avatar>
-                <Typography variant="h6">{publisherName}</Typography>
-              </Stack>
-              <Stack direction="row" spacing={1} alignItems="center">
-                <LocationOnIcon />
-                <Typography variant="h6">{event?.location}</Typography>
-              </Stack>
-              <Stack direction="row" spacing={1} alignItems="center">
-                <Typography variant="h6">
-                  {event?.ticketsAmount
-                    ? event?.ticketsAmount + " tickets avilable " + (event?.ticketPrice && (event?.ticketPrice + ' NIS'))
-                    : "No avilable tickets"}
-                </Typography>
-              </Stack>
-              <Stack direction="row" spacing={1} alignItems="center">
-                <EventIcon />
-                <Typography variant="h6">
-                  {event?.timeAndDate &&
+                  {" "}
+                  <QrCodeScannerIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
+            </Stack>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <EventIcon />
+              <Typography variant="h6">
+                {event?.timeAndDate &&
                     new Date(event?.timeAndDate).toDateString()}
-                </Typography>
-              </Stack>
-            <Divider color={"grey"} variant="middle" />
-            <Typography>
-              {event?.description}
+              </Typography>
+            </Stack>
+            <Stack direction="row" spacing={1} alignItems="center">
+              <LocationOnIcon />
+              <Typography variant="h6">{event?.location}</Typography>
+            </Stack>
+              <Typography variant="h6">
+                {event?.ticketsAmount
+                  ? `${event?.ticketsAmount} tickets avilable`
+                  : "No avilable tickets"}
+              </Typography>
+            <Typography variant="body1">
+              {event?.ticketPrice && `${event?.ticketPrice} ₪`}
             </Typography>
+            <Divider color={"grey"} variant="middle" />
+            <Typography>{event?.description}</Typography>
 
             {currentUser ? (
               <div>
-                {event?.ticketsAmount ? <PaymentForm amount={20} description={event?.name ?? "Event"} /> : <></>}
+                {event?.ticketsAmount ? (
+                  <PaymentForm
+                    amount={20}
+                    description={event?.name ?? "Event"}
+                  />
+                ) : (
+                  <></>
+                )}
               </div>
             ) : (
               <Button
--- client/src/hooks/GraphqlClientProvider/GraphqlClientProvider.tsx
+++ client/src/hooks/GraphqlClientProvider/GraphqlClientProvider.tsx
@@ -10,6 +10,7 @@ import { PropsWithChildren } from "react";
 const client = createClient({
   url: process.env.REACT_APP_BACKEND_URL + "/graphql",
   exchanges: [cacheExchange, fetchExchange],
+  requestPolicy: "cache-and-network",
   fetchOptions: {
     credentials: 'include'
   }
