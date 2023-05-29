6ecef7560a8700f1c3e301d7935c6975e758e73e
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2023-05-29 20:19:32 +0300

    fix

--- server/src/graphql/resolvers/user/user.ts
+++ server/src/graphql/resolvers/user/user.ts
@@ -1,52 +1,79 @@
-import { MutationResolvers, QueryResolvers } from "../../typeDefs"
-import { User as UserModel } from "../../../../mongo/models/User"
-import mongoose, { Types } from 'mongoose';
+import { MutationResolvers, QueryResolvers } from "../../typeDefs";
+import { User as UserModel } from "../../../../mongo/models/User";
+import mongoose, { Types } from "mongoose";
 
-
-const FAILED_MUTATION_MESSAGE = "mutation upadteCredit failed"
+const FAILED_MUTATION_MESSAGE = "mutation upadteCredit failed";
 
 const userResolvers: {
-    Query: Pick<QueryResolvers, "user">;
-    Mutation: Pick<MutationResolvers, "updateCredit" | "updateImage">;
+  Query: Pick<QueryResolvers, "user">;
+  Mutation: Pick<MutationResolvers, "updateCredit" | "updateImage">;
 } = {
-    Query: {
-        user: async (parent, args, context, info) => {
-            const { userId } = args;
+  Query: {
+    user: async (parent, args, context, info) => {
+      const { userId } = args;
 
-            const userData = await UserModel.findOne(
-                { _id: new Types.ObjectId(userId)  }
-            )
-            return userData;
-        }
+      const userData = await UserModel.findOne({
+        _id: new Types.ObjectId(userId),
+      })
+        .exec()
+        .then(
+          ({
+            _id,
+            username,
+            password,
+            email,
+            firstName,
+            lastName,
+            refreshToken,
+            isConfirmed,
+            userConfirmation,
+            image,
+          }) => {
+            return {
+              _id: _id.toString(),
+              username,
+              password,
+              email,
+              firstName,
+              lastName,
+              refreshToken,
+              isConfirmed,
+              userConfirmation,
+              image,
+            };
+          }
+        );
+      return userData;
     },
-    Mutation: {
-        updateCredit: async (userId: any, newCredit) => {
-            try {
-                await UserModel.updateOne(
-                    { _id: new Types.ObjectId(userId) },
-                    { $set: { credit: newCredit } }
-                )
-                return { message: "user credit updated succesfully", code: 200 }
-            } catch (error) {
-                console.log("failed with " + error)
-                return { message: FAILED_MUTATION_MESSAGE, code: 500 }
-            }
-        },
-        updateImage: async (parent, args, context, info) => {
-            try {
-                const { userId, image } = args;
+  },
+  Mutation: {
+    updateCredit: async (userId: any, newCredit) => {
+      try {
+        await UserModel.updateOne(
+          { _id: new Types.ObjectId(userId) },
+          { $set: { credit: newCredit } }
+        );
+        return { message: "user credit updated succesfully", code: 200 };
+      } catch (error) {
+        console.log("failed with " + error);
+        return { message: FAILED_MUTATION_MESSAGE, code: 500 };
+      }
+    },
+    updateImage: async (parent, args, context, info) => {
+      try {
+        const { userId, image } = args;
 
-                await UserModel.updateOne(
-                    { _id: new Types.ObjectId(userId) },
-                    { $set: { image: image } }
-                )
-                return { message: "user credit updated succesfully", code: 200 }
-            } catch (error) {
-                console.log("failed with to save image" + error)
-                return { message: FAILED_MUTATION_MESSAGE, code: 500 }
-            }
-        }
-    }
-}
+        await UserModel.updateOne(
+          { _id: new Types.ObjectId(userId) },
+          { $set: { image: image } }
+        );
+        return { message: "user credit updated succesfully", code: 200 };
+      } catch (error) {
+        console.log("failed with to save image" + error);
+        return { message: FAILED_MUTATION_MESSAGE, code: 500 };
+      }
+    },
+  },
+};
 
-export default userResolvers
+export default userResolvers;
