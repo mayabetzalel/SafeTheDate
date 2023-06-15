e6a19342c3d92d542dea22a2724b7abb69b2a287
thcr <talmachlev3399@gmail.com>
2023-06-15 19:37:12 +0300

    add validation

--- client/src/components/CreateEvent.tsx
+++ client/src/components/CreateEvent.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "urql";
-import { useSnackbar } from "notistack";
 import { graphql } from "../graphql";
 import { InputEvent, MutationResponse } from "../graphql/graphql";
 import {
@@ -12,6 +11,7 @@ import {
   Stack,
   Paper,
   InputAdornment,
+  FormHelperText,
 } from "@mui/material";
 import ImagePicker from "./ImagePicker";
 
@@ -29,14 +29,14 @@ export const CreateEvent = () => {
   const eventLocationRef = useRef<HTMLInputElement | null>();
   const eventTimeAndDateRef = useRef<HTMLInputElement | null>();
   const eventTypeRef = useRef<HTMLInputElement | null>();
-  const eventTicketAmoutRef = useRef<HTMLInputElement | null>();
+  const eventTicketAmountRef = useRef<HTMLInputElement | null>();
   const eventDescriptionRef = useRef<HTMLInputElement | null>();
   const eventTicketPriceRef = useRef<HTMLInputElement | null>();
   const [image, setImage] = useState(undefined);
-  const { enqueueSnackbar } = useSnackbar();
+  const [formErrors, setFormErrors] = useState({});
   const navigate = useNavigate();
 
-  const [{error}, createEvent] = useMutation<{
+  const [{ error }, createEvent] = useMutation<{
     createEvent: MutationResponse;
   }>(CREATE_EVENT_MUTATION);
 
@@ -45,16 +45,76 @@ export const CreateEvent = () => {
   };
 
   const handleDiscard = () => {
-    if (eventDescriptionRef.current) eventDescriptionRef.current.value = '';
-    if (eventLocationRef.current) eventLocationRef.current.value = '';
-    if (eventNameRef.current) eventNameRef.current.value = '';
-    if (eventTicketAmoutRef.current) eventTicketAmoutRef.current.value = '';
-    if (eventTicketPriceRef.current) eventTicketPriceRef.current.value = '';
-    if (eventTimeAndDateRef.current) eventTimeAndDateRef.current.value = '';
-    if (eventTypeRef.current) eventTypeRef.current.value = '';
+    if (eventDescriptionRef.current) eventDescriptionRef.current.value = "";
+    if (eventLocationRef.current) eventLocationRef.current.value = "";
+    if (eventNameRef.current) eventNameRef.current.value = "";
+    if (eventTicketAmountRef.current) eventTicketAmountRef.current.value = "";
+    if (eventTicketPriceRef.current) eventTicketPriceRef.current.value = "";
+    if (eventTimeAndDateRef.current) eventTimeAndDateRef.current.value = "";
+    if (eventTypeRef.current) eventTypeRef.current.value = "";
   };
 
+  function validateForm() {
+    const errors = {};
+
+    if (!eventNameRef.current?.value) {
+      errors["eventName"] = "Please enter the name of the event";
+    }
+
+    if (!eventLocationRef.current?.value) {
+      errors["eventLocation"] = "Please enter the location of the event";
+    }
+
+    if (!eventTimeAndDateRef.current?.value) {
+      errors["eventTimeAndDate"] = "Please enter the time and date of the event";
+    }
+
+    if (eventTimeAndDateRef.current?.value === "") {
+      errors["eventTimeAndDate"] = "Event date and time are required";
+    } else {
+      const selectedDateTime = new Date(eventTimeAndDateRef.current?.value || 0);
+      const currentDateTime = new Date();
+
+      if (selectedDateTime <= currentDateTime) {
+        errors["eventTimeAndDate"] = "Event date and time should be in the future";
+      } else if (
+        selectedDateTime.getTime() - currentDateTime.getTime() <
+        60 * 60 * 1000 // 1 hour in milliseconds
+      ) {
+        errors["eventTimeAndDate"] =
+          "Event should be scheduled at least 1 hour from now";
+      }
+    }    
+
+    if (!eventTypeRef.current?.value) {
+      errors["eventType"] = "Please enter the type of the event";
+    }
+
+    if (!eventTicketAmountRef.current?.value) {
+      errors["eventTicketAmount"] = "Please enter the ticket amount";
+    }
+
+    if (!eventTicketPriceRef.current?.value) {
+      errors["eventTicketPrice"] = "Please enter the ticket price";
+    }
+
+    setFormErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  }
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+  
+    // Clear the error message for the corresponding input field
+    setFormErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
+  }
+
   function handleEventCreation() {
+    if (!validateForm()) {
+      return;
+    }
+
     const inputEvent: InputEvent = {
       name: eventNameRef.current?.value || "",
       location: eventLocationRef.current?.value || "",
@@ -62,26 +122,29 @@ export const CreateEvent = () => {
         eventTimeAndDateRef.current?.value || new Date().toString()
       ),
       type: eventTypeRef.current?.value || "",
-      ticketsAmount: eventTicketAmoutRef.current?.value
-        ? parseInt(eventTicketAmoutRef.current?.value)
+      ticketsAmount: eventTicketAmountRef.current?.value
+        ? parseInt(eventTicketAmountRef.current?.value)
         : 1,
-      description: eventDescriptionRef.current?.value
-        ? eventDescriptionRef.current?.value
-        : "",
-      ticketPrice: eventTicketPriceRef.current?.value ? parseInt(eventTicketPriceRef.current?.value) : 1,
+      description: eventDescriptionRef.current?.value || "",
+      ticketPrice: eventTicketPriceRef.current?.value
+        ? parseInt(eventTicketPriceRef.current?.value)
+        : 1,
       image: image || "",
     };
 
     // Call the createEvent mutation with the inputEvent object
     createEvent({ inputEvent }).then((result) => {
-      if (result.data?.createEvent.code == 500) {
+      if (result.data?.createEvent.code === 500) {
         console.error("Error creating event:", result.error);
-        enqueueSnackbar("An error occurred", { variant: "error" });
+        setFormErrors({
+          general: "An error occurred",
+        });
       } else if (result.error?.message.includes("Auth failed")) {
-        enqueueSnackbar("Token expired, Please login again or refresh", {variant: "error"});
+        setFormErrors({
+          general: "Token expired, Please login again or refresh",
+        });
       } else {
         navigate("/");
-        enqueueSnackbar("Event created successfully", { variant: "success" });
         console.log("Event created:", result.data?.createEvent);
       }
     });
@@ -89,86 +152,134 @@ export const CreateEvent = () => {
 
   return (
     <>
-      <Typography variant="h3" align="center" gutterBottom color={"white"}>
+      <Typography variant="h3" align="center" gutterBottom color="white">
         Create The Next Event
       </Typography>
       <Grid container padding={10} columnSpacing={2}>
         <Grid item xl={3} md={12} sm={12} xs={12}>
-          <ImagePicker image={image} onChangeImage={onChangeImage} buttonTitle={"Pick an image"} />
+          <ImagePicker
+            image={image}
+            onChangeImage={onChangeImage}
+            buttonTitle={"Pick an image"}
+          />
         </Grid>
         <Grid item container spacing={2} xl={9} md={12} sm={12} xs={9}>
-          <Grid item xl={12} md={12} sm={12} xs={12}>
-            <TextField
-              fullWidth
-              label="Name of Event"
-              color={"secondary"}
-              variant="outlined"
-              inputRef={eventNameRef}
-            />
+          <Grid container spacing={2}>
+            <Grid item xl={12} md={12} sm={12} xs={12}>
+              <TextField
+                fullWidth
+                label="Name of Event"
+                color="secondary"
+                variant="outlined"
+                inputRef={eventNameRef}
+                error={Boolean(formErrors["eventName"])}
+                helperText={formErrors["eventName"]}
+                onChange={handleInputChange}
+                name="eventName"
+              />
+            </Grid>
+            <Grid item xl={6} md={6} sm={12} xs={12}>
+              <TextField
+                fullWidth
+                label="Location"
+                color="secondary"
+                inputRef={eventLocationRef}
+                variant="outlined"
+                error={Boolean(formErrors["eventLocation"])}
+                helperText={formErrors["eventLocation"]}
+                onChange={handleInputChange}
+                name="eventLocation"
+              />
+            </Grid>
+            <Grid item xl={6} md={6} sm={12} xs={12}>
+              <TextField
+                fullWidth
+                label="Time and Date"
+                color="secondary"
+                inputRef={eventTimeAndDateRef}
+                variant="outlined"
+                type="datetime-local"
+                error={Boolean(formErrors["eventTimeAndDate"])}
+                helperText={formErrors["eventTimeAndDate"]}
+                onChange={handleInputChange}
+                name="eventTimeAndDate"
+              />
+            </Grid>
+            <Grid item xl={6} md={6} sm={12} xs={12}>
+              <TextField
+                fullWidth
+                color="secondary"
+                label="Type of Event"
+                inputRef={eventTypeRef}
+                variant="outlined"
+                error={Boolean(formErrors["eventType"])}
+                helperText={formErrors["eventType"]}
+                onChange={handleInputChange}
+                name="eventType"
+              />
+            </Grid>
+            <Grid item xl={3} md={3} sm={6} xs={12}>
+              <TextField
+                fullWidth
+                color="secondary"
+                label="Tickets amount"
+                inputRef={eventTicketAmountRef}
+                variant="outlined"
+                type="number"
+                error={Boolean(formErrors["eventTicketAmount"])}
+                helperText={formErrors["eventTicketAmount"]}
+                onChange={handleInputChange}
+                name="eventTicketAmount"
+              />
+            </Grid>
+            <Grid item xl={3} md={3} sm={6} xs={12}>
+              <TextField
+                fullWidth
+                color="secondary"
+                label="Tickets Price"
+                inputRef={eventTicketPriceRef}
+                variant="outlined"
+                type="number"
+                error={Boolean(formErrors["eventTicketPrice"])}
+                helperText={formErrors["eventTicketPrice"]}
+                onChange={handleInputChange}
+                name="eventTicketPrice"
+              />
+            </Grid>
+            <Grid item xl={12} md={12} sm={12} xs={12}>
+              <TextField
+                fullWidth
+                multiline
+                color="secondary"
+                label="Description"
+                inputRef={eventDescriptionRef}
+                variant="outlined"
+                onChange={handleInputChange}
+                name="eventDescription"
+              />
+            </Grid>
           </Grid>
-          <Grid item xl={6} md={6} sm={12} xs={12}>
-            <TextField
-              fullWidth
-              label="Location"
-              color={"secondary"}
-              inputRef={eventLocationRef}
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xl={6} md={6} sm={12} xs={12}>
-            <TextField
-              fullWidth
-              label="Time and Date"
-              color={"secondary"}
-              inputRef={eventTimeAndDateRef}
-              variant="outlined"
-              type="datetime-local"
-            />
-          </Grid>
-          <Grid item xl={6} md={6} sm={12} xs={12}>
-            <TextField
-              fullWidth
-              color={"secondary"}
-              label="Type of Event"
-              inputRef={eventTypeRef}
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xl={3} md={3} sm={6} xs={12}>
-            <TextField
-              fullWidth
-              color={"secondary"}
-              label="Tickets amount"
-              inputRef={eventTicketAmoutRef}
-              variant="outlined"
-              type="number"
-            />
-          </Grid>
-          <Grid item xl={3} md={3} sm={6} xs={12}>
-            <TextField
-              fullWidth
-              color={"secondary"}
-              label="Tickets Price"
-              inputRef={eventTicketPriceRef}
-              variant="outlined"
-              type="number"
-            />
-          </Grid>
-          <Grid item xl={12} md={12} sm={12} xs={12}>
-            <TextField
-              fullWidth
-              multiline
-              color={"secondary"}
-              label="Description"
-              inputRef={eventDescriptionRef}
-              variant="outlined"
-            />
-          </Grid>
+
         </Grid>
       </Grid>
-      <Grid container justifyContent={"space-around"}>
+      {formErrors["general"] && (
+        <Typography
+          variant="body2"
+          color="error"
+          align="center"
+          gutterBottom
+        >
+          {formErrors["general"]}
+        </Typography>
+      )}
+      <Grid container justifyContent="space-around">
         <Grid item xs={1}>
-          <Button fullWidth variant="outlined" color="secondary" onClick={handleDiscard}>
+          <Button
+            fullWidth
+            variant="outlined"
+            color="secondary"
+            onClick={handleDiscard}
+          >
             Discard
           </Button>
         </Grid>
